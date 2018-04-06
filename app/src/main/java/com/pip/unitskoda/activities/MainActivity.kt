dab5825e63e7ab0fe30d80486eba0854dd6da96f
Lukas Kusik <lukas.kusik@gmail.com>
2018-04-06 11:22:31 +0200

    Main activity

---
+++ app/src/main/java/com/pip/unitskoda/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pip.unitskoda.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.pip.unitskoda.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
